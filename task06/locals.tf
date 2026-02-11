fbfe7c0947aa1228f7eb50e5d8b2b5cdbc20e8cf
Yaşar Mehmet Bağdatlı <117036541+yasarmb@users.noreply.github.com>
2026-02-11 08:25:46 +0300

    Delete task06 directory

--- task06/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  rg_name           = join("", [var.name_prefix, "rg"])
-  sql_server_name   = join("", [var.name_prefix, "sql"])
-  sql_db_name       = join("", [var.name_prefix, "sql-db"])
-  asp_name          = join("", [var.name_prefix, "asp"])
-  app_name          = join("", [var.name_prefix, "app"])
-}
