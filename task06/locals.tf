8bb193e62ddd48ffa382da9d45cbb12149a1caf8
Yaşar Mehmet Bağdatlı <117036541+yasarmb@users.noreply.github.com>
2026-02-11 08:22:55 +0300

    Rename sql_database_name to sql_db_name

--- task06/locals.tf
+++ task06/locals.tf
@@ -1,7 +1,7 @@
 locals {
   rg_name           = join("", [var.name_prefix, "rg"])
   sql_server_name   = join("", [var.name_prefix, "sql"])
-  sql_database_name = join("", [var.name_prefix, "sql-db"])
+  sql_db_name = join("", [var.name_prefix, "sql-db"])
   asp_name          = join("", [var.name_prefix, "asp"])
   app_name          = join("", [var.name_prefix, "app"])
 }
