2614ebe9a5508411cea76e38d59256053f1da679
Yaşar Mehmet Bağdatlı <117036541+yasarmb@users.noreply.github.com>
2026-02-11 09:21:16 +0300

    bugs fixed

---
+++ task06/modules/sql/main.tf
@@ -0,0 +1,54 @@
+resource "random_password" "db_password" {
+  length  = 10
+  special = true
+  upper   = true
+}
+
+
+resource "azurerm_mssql_server" "main" {
+  name                         = var.sql_server_name
+  resource_group_name          = var.rg_name
+  location                     = var.region
+  version                      = "12.0"
+  administrator_login          = var.db_username
+  administrator_login_password = random_password.db_password.result
+  tags                         = var.tags
+
+}
+
+resource "azurerm_mssql_database" "main" {
+  name      = var.sql_db_name
+  server_id = azurerm_mssql_server.main.id
+  sku_name  = var.sql_database_service_model
+  tags      = var.tags
+}
+
+resource "azurerm_mssql_firewall_rule" "specified" {
+  name             = var.sql_server_firewall_rule_name_specified
+  server_id        = azurerm_mssql_server.main.id
+  start_ip_address = var.allowed_ip_address
+  end_ip_address   = var.allowed_ip_address
+}
+
+resource "azurerm_mssql_firewall_rule" "azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_mssql_server.main.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+
+}
+
+resource "azurerm_key_vault_secret" "db-username" {
+  name         = var.key_vault_secret_name_for_sql_admin_name
+  value        = azurerm_mssql_server.main.administrator_login
+  key_vault_id = var.key-value-id
+  tags         = var.tags
+}
+
+resource "azurerm_key_vault_secret" "password" {
+  name         = var.key_vault_secret_name_for_sql_admin_password
+  value        = azurerm_mssql_server.main.administrator_login_password
+  key_vault_id = var.key-value-id
+  tags         = var.tags
+}
+
---
+++ task06/terraform.tfvars
@@ -0,0 +1,23 @@
+region                                       = "East US"
+existing_key_vault_resource_group_name       = "cmaz-wxu9jawu-mod6-kv-rg"
+existing_key_vault_name                      = "cmaz-wxu9jawu-mod6-kv"
+rg_name                                      = "cmaz-wxu9jawu-mod6-rg"
+key_vault_secret_name_for_sql_admin_name     = "sql-admin-name"
+key_vault_secret_name_for_sql_admin_password = "sql-admin-password"
+sql_server_name                              = "cmaz-wxu9jawu-mod6-sql"
+sql_db_name                                  = "cmaz-wxu9jawu-mod6-sql-db"
+sql_database_service_model                   = "S2"
+sql_server_firewall_rule_name                = "AllowAzureServices"
+asp_name                                     = "cmaz-wxu9jawu-mod6-asp"
+app_service_plan_sku                         = "P0v3"
+
+tags = {
+  Creator = "yasarmehmet_bagdatli@epam.com"
+}
+
+allowed_ip_address                      = "18.153.146.156"
+db_username                             = "sqladmin"
+name_prefix                             = "cmaz-wxu9jawu-mod6-"
+sql_server_firewall_rule_name_specified = "allow-verification-ip"
+app_name                                = "cmaz-wxu9jawu-mod6-app"
+web_application_dotnet_version          = "8.0"
